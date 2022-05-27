428a7c6ec6a5cc6dad03289cd35f3437e676b639
OrkWard <orkward_o@outlook.com>
2022-05-27 12:14:54 +0800

    添加注释

--- MapEver_server/views.py
+++ MapEver_server/views.py
@@ -1,8 +1,12 @@
 from django.http import FileResponse, HttpResponse
-from rest_framework import viewsets
+from django.core.files import File
+from rest_framework import viewsets, status
 from rest_framework.decorators import action, api_view
+from rest_framework.response import Response
 from .serializers import FileSerializer
 from .models import upload_file
+import geopandas as gpd
+import os
 
 def index(request):
     return HttpResponse('<p>Hello! MapEver here.<p><a href=\"https://github.com/OrkWard/WebGIS_Project\">GitHub<a>')
@@ -19,7 +23,26 @@ class data(viewsets.ModelViewSet):
 
 @api_view(['PUT'])
 def overlay(request):
+    # 请求为一个dict，包含了操作文件所需的信息
     how = request.data['how']
-    # if how in ['intersection', 'union', 'identity', 'symmetric_difference', 'differnce']:
-    
-    return HttpResponse('overlay finish!' + str(request.data))
+    if how in ['intersection', 'union', 'identity', 'symmetric_difference', 'differnce']:
+        # geopandas操作
+        source_file = upload_file.objects.get(id=request.data['source']).data
+        source = gpd.read_file(source_file)
+        object_file = upload_file.objects.get(id=request.data['object']).data
+        object = gpd.read_file(object_file)
+        result = gpd.overlay(source, object, how=how)
+        # 该文件为临时文件，记得删除，数据库存取时会自动向指定目录写入
+        result.to_file(request.data['output'], driver='GeoJSON')
+
+        # 数据库操作和返回请求，模仿即可
+        with open(request.data['output'], 'rb') as f:
+            # 写入
+            new_record = upload_file(name=request.data['output'], form='vector', data=File(f))
+            new_record.save()
+        # 线性化
+        serializer = FileSerializer(upload_file.objects.get(pk=new_record.id))
+        # 删除临时文件
+        os.remove(request.data['output'])
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
